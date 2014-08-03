e47c23f307909855b351ae7c16e13ad2f69f3149
Max <titantbx1989215@gmail.com>
2014-08-03 17:17:28 +1200

    mean and median

--- median.py
+++ median.py
@@ -23,38 +23,38 @@ def write_pixel(pixel, img, row, col):
         row += 1
     return img
 
-def scan_image(img, col, row, operator):
+def scan_image(img, row, col):
     for i in xrange(row - 3):
         for j in xrange(col - 3):
             pixel = [[0 for x in xrange(3)] for x in xrange(3)]
             get_pixel(pixel, img, i, j)
-            img = mean(operator, pixel, img, i, j)
+            img = median(pixel, img, i, j)
     return img
 
-def mean(operator, pixel, img, row, col):
-    gsum = 0
+def median(pixel, img, row, col):
+    pixel[1][1] = sort(pixel)
+    img = write_pixel(pixel, img, row, col)
+    return img
+
+def sort(pixel):
+    a = []
     for i in xrange(3):
         for j in xrange(3):
-            if i == 1 and j == 1:
-                continue
-            gsum += pixel[i][j] * operator[i][j]
+            a.append(pixel[i][j])
+    a = sorted(a)
+    return a[4]
 
-    pixel[1][1] = gsum / 8
-    write_pixel(pixel, img, row, col)
-    return img
 if __name__ == "__main__":
     im = Image.open(sys.argv[1])
     width = im.size[0]
     height = im.size[1]
     imarray = numpy.array(im)
+
     my_im = Image.new("L", (width, height))
     my_im_array = numpy.array(my_im)
-    operator = [
-                [1, 1, 1],
-                [1, 1, 1],
-                [1, 1, 1]
-            ]
-    my_im_array = scan_image(imarray, width, height, operator)
+
+    my_im_array = scan_image(imarray, height, width)
 
     mytiff = Image.fromarray(my_im_array)
+
     mytiff.save(sys.argv[2])
