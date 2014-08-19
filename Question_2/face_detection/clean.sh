b2c49f9fd545ab84518c20d364bbfa622f3589a7
Max <titantbx1989215@gmail.com>
2014-08-19 14:20:01 +1200

    delete some bla

--- Question_2/face_detection/clean.sh
+++ Question_2/face_detection/clean.sh
@@ -1,7 +1,8 @@
 #!/usr/pkg/bin/tcsh
-
-rm result/tp_fp.csv
-rm result/training/*
-rm result/testing/*
-rm result/roc.png
-rm result/Rplots.pdf
+foreach i (1 2)
+	rm result/classifier_${i}/tp_fp.csv
+	rm result/classifier_${i}/training/*
+	rm result/classifier_${i}/testing/*
+	rm result/roc.png
+	rm result/Rplots.pdf
+end
--- Question_2/face_detection/run.sh
+++ Question_2/face_detection/run.sh
@@ -4,18 +4,37 @@ if -e result/tp_fp.csv then
 endif
 
 foreach i (1 2 3 4 5)
-	python ./src/feature_extraction.py training_sample/face_sample_${i}/ training
-	python ./src/feature_extraction.py training_sample/non_face_${i}/ training
-	python ./src/feature_extraction.py test_sample/face_sample_${i}/ testing
-	python ./src/feature_extraction.py test_sample/non_face_${i}/ testing
-	python ./src/cal_mean_and_var.py result/training/ face_sample_${i}_dataset.csv
-	python ./src/cal_mean_and_var.py result/training/ non_face_${i}_dataset.csv
+	python ./src/classifier_1/feature_extraction.py training_sample/face_sample_0${i}/ training
+	python ./src/classifier_1/feature_extraction.py training_sample/non_face_0${i}/ training
+	python ./src/classifier_1/feature_extraction.py test_sample/face_sample_0${i}/ testing
+	python ./src/classifier_1/feature_extraction.py test_sample/non_face_0${i}/ testing
+	python ./src/classifier_1/cal_mean_and_var.py result/classifier_1/training/ face_sample_0${i}_dataset.csv
+	python ./src/classifier_1/cal_mean_and_var.py result/classifier_1/training/ non_face_0${i}_dataset.csv
+end
 
-
+foreach i (1 2 3 4 5)
 	foreach j (1 2 3 4 5)
-		python src/naive_bayes.py result/testing/face_sample_${j}_dataset.csv result/testing/non_face_${j}_dataset.csv result/training/mean_face_sample_${i}_dataset.csv result/training/mean_non_face_${i}_dataset.csv
+	python src/classifier_1/naive_bayes.py result/classifier_1/testing/face_sample_0${j}_dataset.csv result/classifier_1/testing/non_face_0${j}_dataset.csv result/classifier_1/training/mean_face_sample_0${i}_dataset.csv result/classifier_1/training/mean_non_face_0${i}_dataset.csv
 	end
 end
+
+
+foreach i (1 2 3 4 5)
+	python ./src/classifier_2/feature_extraction.py training_sample/face_sample_0${i}/ training
+	python ./src/classifier_2/feature_extraction.py training_sample/non_face_0${i}/ training
+	python ./src/classifier_2/feature_extraction.py test_sample/face_sample_0${i}/ testing
+	python ./src/classifier_2/feature_extraction.py test_sample/non_face_0${i}/ testing
+	python ./src/classifier_2/cal_mean_and_var.py result/classifier_2/training/ face_sample_0${i}_dataset.csv
+	python ./src/classifier_2/cal_mean_and_var.py result/classifier_2/training/ non_face_0${i}_dataset.csv
+end
+
+foreach i (1 2 3 4 5)
+	foreach j (1 2 3 4 5)
+	python src/classifier_2/naive_bayes.py result/classifier_2/testing/face_sample_0${j}_dataset.csv result/classifier_2/testing/non_face_0${j}_dataset.csv result/classifier_2/training/mean_face_sample_0${i}_dataset.csv result/classifier_2/training/mean_non_face_0${i}_dataset.csv
+	end
+end
+
+
 
 Rscript src/roc.r
 mv Rplots.pdf result/
